9231ca29268520ec1f56513083c5ef9cb7f8001c
Seva Gavrilov <gavrilovseva@gmail.com>
2018-01-26 23:49:56 -0300

    Agregado glyphs.

---
+++ Assets/Scripts/Entities/GlyphButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlyphButton : MonoBehaviour {
+
+	public string name;
+
+	private bool clicked;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnMouseUp() {
+		Debug.Log("Soy " + this.gameObject.name + "!");
+	}
+
+}
---
+++ Assets/Scripts/GoToScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToScene : MonoBehaviour {
+	
+	public string Destination = "";
+
+	public void GoTo() {
+		Application.LoadLevel (Destination);
+	}
+
+	void OnMouseDown() {
+		GoTo ();
+	}
+
+}
