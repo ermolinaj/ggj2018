4d252d38f9543a80f31dace1c1925694201388f6
Agustin Borgna <hello@aborgna.com.ar>
2018-01-27 12:52:48 -0300

    lookAt script

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+
+	public Transform target;
+	public float yawVariation = 0.1f;
+
+	float yawOffset;
+
+	void Start() {
+		yawOffset = Random.Range(-yawVariation,yawVariation);
+	}
+
+	void Update () {
+		transform.LookAt(target);
+		transform.Rotate(0,yawOffset,0);
+	}
+}
