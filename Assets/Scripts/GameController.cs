f1aea2ad999d3ccaaeae5d1c1c2911bfeb6071e3
Seva Gavrilov <gavrilovseva@gmail.com>
2018-01-26 23:25:29 -0300

    Agregado game controller inteligentisimo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+	public int persons = 20;
+	public GameObject person;
+
 	// Use this for initialization
 	void Start () {
+		
+		for (var y = 0; y < persons; y++) {
+			var vector = new Vector2 (Random.Range (0, 10), Random.Range (0, 10));
+			Instantiate (person, vector, Quaternion.identity);
+		}
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		GameObject[] persons = GameObject.FindGameObjectsWithTag("Person");
+
+		//Debug.Log (persons.Length);
+
 	}
 }
