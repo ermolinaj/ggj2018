efab49916daa324ce947c45938ffbb3e09cc540f
Seva Gavrilov <gavrilovseva@gmail.com>
2018-01-27 17:16:30 -0300

    Agrego un log super pedorro.

---
+++ Assets/Scripts/Entities/GlyphTextBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlyphTextBoard : MonoBehaviour {
+
+	Text txt;
+	public static GlyphTextBoard instance = null;
+
+	List<char> representation;
+
+	void Awake() {
+		if(instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy(gameObject);
+	}
+
+	public void setRepresentation(List<char> representation) {
+		this.representation = representation;
+	}
+
+	void Start () {
+		txt = gameObject.GetComponent<Text>();
+		txt.text = "hola wachines\n";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void showGlyphs(List<List<int>> secuences) {
+		string t = "";
+		foreach (List<int> s in secuences) {
+			foreach (int i in s) {
+				t += representation [i];
+			}
+			t += '\n';
+		}
+		txt.text = t;
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
@@ -13,7 +14,11 @@ public class GameController : MonoBehaviour {
 
 	public TraitController traitController;
 
-	List<GlyphSequence> sequences = new List<GlyphSequence>();
+	List<char> symbols = new List<char> {'△', '□', 'X', 'O', 'A', 'B', 'C'};
+	List<char> symbolsToUse;
+
+	List<List<int>> glyphIdSequences = new List<List<int>>();
+	List<int> currentGlyphIdSequence = new List<int> ();
 	List<GlyphType> glyphOrder;
 
 	bool waitingForGlyphs = false;
@@ -34,6 +39,11 @@ public class GameController : MonoBehaviour {
 		// Generate the glyphOrder
 		glyphOrder = new List<GlyphType>()
 			{GlyphType.Hat, GlyphType.Poncho, GlyphType.Action};
+		symbolsToUse = symbols.OrderBy (x => Random.Range(0, 100)).Take (maxGlyphs).ToList();
+		Debug.Log(new string(symbolsToUse.ToArray()));
+
+		GlyphTextBoard.instance.setRepresentation (symbolsToUse);
+
 		waitingForGlyphs = true;
 	}
 
@@ -76,6 +86,7 @@ public class GameController : MonoBehaviour {
 		}
 
 		currGlyphInSeq++;
+		currentGlyphIdSequence.Add (glyphId);
 		if(currGlyphInSeq >= glyphOrder.Count)
 			CompleteGlyphSequence();
 	}
@@ -84,9 +95,12 @@ public class GameController : MonoBehaviour {
 		Debug.Log("Completed a new sequence");
 		traitController.TransitionTraits(currSequence);
 
-		sequences.Add(currSequence);
+		glyphIdSequences.Add (currentGlyphIdSequence);
+		currentGlyphIdSequence = new List<int> ();
 		currSequence = new GlyphSequence();
 		currGlyphInSeq = 0;
+	
+		GlyphTextBoard.instance.showGlyphs (glyphIdSequences);
 	}
 
 }
