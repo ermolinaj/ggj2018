c577a3637c369111372f3639ff76f204d86a0a6c
Agustin Borgna <hello@aborgna.com.ar>
2018-01-28 13:53:31 -0300

    scene transitions

---
+++ Assets/Scripts/ClickToScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ClickToScene : MonoBehaviour {
+
+	public CanvasFader fader;
+	public string nextScene;
+
+	void Update() {
+		if(Input.GetButtonDown("Skip")) {
+			if(fader != null) {
+				fader.FadeOut();
+				Invoke("gotoScene", fader.fadeOutTime);
+			} else {
+				gotoScene();
+			}
+		}
+	}
+
+	void gotoScene() {
+		SceneManager.LoadScene(nextScene);
+	}
+}
