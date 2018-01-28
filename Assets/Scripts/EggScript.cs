8dd8700eb85d4a2518dac2e134da13aca5f04617
Agustin Borgna <hello@aborgna.com.ar>
2018-01-27 23:28:31 -0300

    fix egg

---
+++ Assets/Scripts/EggScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.CompilerServices;
+
+public class EggScript : MonoBehaviour
+{
+
+	public float Speed = 10;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 forward = this.transform.forward;
+		forward.y = 0f;
+
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+
+		this.transform.position += vertical * forward * Speed * Time.deltaTime;
+
+		this.transform.RotateAround (this.transform.position,
+			horizontal * Vector3.up, 6 * Speed * Time.deltaTime);
+	}
+}
